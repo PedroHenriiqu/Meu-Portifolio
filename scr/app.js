7100beb05b8778e97b2497de879c006d04ebe038
Pedro Sousa <pompermayerpedrohenrique@gmail.com>
2024-04-21 19:30:34 -0300

    Modificações finais, primeira versão

--- scr/app.js
+++ scr/app.js
@@ -1,9 +1,15 @@
-const nomeApp = {
-    data() {
-        return {
-            nome: "Pedro"
-        }
-    }
-}
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
 
-Vue.createApp(nomeApp).mount("#app");
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
